3193811f772c9180ffeaa54539ab779276bfbf9a
salgadojorge <53006033+salgadojorge@users.noreply.github.com>
2019-08-29 14:49:18 -0500

    Delete insertar_datos_especie.sql

--- insertar_datos_especie.sql
+++
@@ -1,8 +0,0 @@
-insert into especie values(0,'Canino');
-insert into especie values(1,'Felino');
-insert into especie values(2,'Aves');
-insert into especie values(3,'Reptiles');
-insert into especie values(4,'Roedores');
-insert into especie values(5,'Equinos');
-
-commit;
