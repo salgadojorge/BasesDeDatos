88d84d94b4b11cd98cfd7d348375b4a137539a23
salgadojorge <53006033+salgadojorge@users.noreply.github.com>
2019-08-29 14:51:24 -0500

    Delete Insertar_datos_especialidad.sql

--- Insertar_datos_especialidad.sql
+++
@@ -1,6 +0,0 @@
-insert into especialidad values(1,'Neurologia',1.10);
-insert into especialidad values(2,'Cardiologia',1.15);
-insert into especialidad values(3,'Odontologia',1.82);
-insert into especialidad values(4,'Ortopedia',1.76);
-insert into especialidad values(5,'Esterilizacion',1.15);
-
