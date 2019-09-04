f0ae4e0f981ba88e3dc5f825a40c318c46ef78fa
salgadojorge <53006033+salgadojorge@users.noreply.github.com>
2019-09-03 20:55:23 -0500

    quitar views inecesarios

--- Consultas/consulta_5.sql
+++ Consultas/consulta_5.sql
@@ -1,24 +1,7 @@
---VISTA PARA ACTUALIZAR CONSULTAS
-create view update_c
-as  with consultas_p as (
-select *
-from visitas natural join veterinario natural join especialidad
-where tipo like 'CONSULTA')
-select id_visita,factor_costo*50000 as "COSTO"
-from consultas_p;
---VISTA PARA ACTUALIZAR HOSPITALIZACION
-create view update_H
-as 
-with hospitalizaciones_p as (
-select *
-from visitas natural join veterinario natural join especialidad
-where tipo like 'HOSPITALIZACION')
-select id_visita,tipo,fecha_ingreso,fecha_egreso,motivo_visita,id_mascota,numero_doc_vet,tipo_doc_vet,-(fecha_ingreso - (case when fecha_egreso is null then to_date(sysdate) else fecha_egreso end))*factor_costo*70000 as "COSTO" 
-from hospitalizaciones_p;
-commit;
 --ACTUALIZACION DE COSTOS EN LA TABLA PRINCIPAL DE VISITAS
 update visitas
 set visitas.costo =
 ((select update_h.costo from update_h  where update_h.id_visita = visitas.id_visita) union
 (select update_c.costo from update_c  where update_c.id_visita = visitas.id_visita) );
 --commit;
+rollback;
