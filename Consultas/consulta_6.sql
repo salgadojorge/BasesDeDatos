baafb13c203abf2f7a59b34e823f645c21bace43
salgadojorge <53006033+salgadojorge@users.noreply.github.com>
2019-09-03 20:47:52 -0500

    Update consulta_6.sql

--- Consultas/consulta_6.sql
+++ Consultas/consulta_6.sql
@@ -1,7 +1,7 @@
 --consulta 6
 
 insert into visitas
-values( 7,'CONSULTA', to_date(sysdate), to_date(sysdate), 'Perro inseguro del query', 1, 1420132569, 'CC', null  );
+values( 11,'CONSULTA', to_date(sysdate), to_date(sysdate), 'Perro inseguro del query', 1, 1420132569, 'CC', null  );
 
 insert into notas(id_visita,fecha_nota, nota)
 select visitas.id_visita, to_date(sysdate),concat('NOTA AUTOMATICA',concat(' ',to_date(sysdate)))
